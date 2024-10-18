2e86eab5385ee536b0e0b1a57f053e88d7d1a37c
Eubooks3003 <ellina.zhang@mail.utoronto.ca>
2024-10-18 00:46:02 -0400

    Added changed json

---
+++ robomimic/exps/templates/dataset_size.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+# ==========dataset_size==========
+
+#  task: lift
+#    dataset type: ph/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/ph/20_percent/low_dim/bc_rnn.json
+
+#  task: lift
+#    dataset type: ph/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/ph/20_percent/image/bc_rnn.json
+
+#  task: lift
+#    dataset type: ph/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/ph/50_percent/low_dim/bc_rnn.json
+
+#  task: lift
+#    dataset type: ph/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/ph/50_percent/image/bc_rnn.json
+
+#  task: lift
+#    dataset type: mh/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/mh/20_percent/low_dim/bc_rnn.json
+
+#  task: lift
+#    dataset type: mh/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/mh/20_percent/image/bc_rnn.json
+
+#  task: lift
+#    dataset type: mh/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/mh/50_percent/low_dim/bc_rnn.json
+
+#  task: lift
+#    dataset type: mh/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/lift/mh/50_percent/image/bc_rnn.json
+
+#  task: can
+#    dataset type: ph/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/ph/20_percent/low_dim/bc_rnn.json
+
+#  task: can
+#    dataset type: ph/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/ph/20_percent/image/bc_rnn.json
+
+#  task: can
+#    dataset type: ph/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/ph/50_percent/low_dim/bc_rnn.json
+
+#  task: can
+#    dataset type: ph/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/ph/50_percent/image/bc_rnn.json
+
+#  task: can
+#    dataset type: mh/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/mh/20_percent/low_dim/bc_rnn.json
+
+#  task: can
+#    dataset type: mh/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/mh/20_percent/image/bc_rnn.json
+
+#  task: can
+#    dataset type: mh/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/mh/50_percent/low_dim/bc_rnn.json
+
+#  task: can
+#    dataset type: mh/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/can/mh/50_percent/image/bc_rnn.json
+
+#  task: square
+#    dataset type: ph/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/ph/20_percent/low_dim/bc_rnn.json
+
+#  task: square
+#    dataset type: ph/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/ph/20_percent/image/bc_rnn.json
+
+#  task: square
+#    dataset type: ph/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/ph/50_percent/low_dim/bc_rnn.json
+
+#  task: square
+#    dataset type: ph/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/ph/50_percent/image/bc_rnn.json
+
+#  task: square
+#    dataset type: mh/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/mh/20_percent/low_dim/bc_rnn.json
+
+#  task: square
+#    dataset type: mh/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/mh/20_percent/image/bc_rnn.json
+
+#  task: square
+#    dataset type: mh/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/mh/50_percent/low_dim/bc_rnn.json
+
+#  task: square
+#    dataset type: mh/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/square/mh/50_percent/image/bc_rnn.json
+
+#  task: transport
+#    dataset type: ph/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/ph/20_percent/low_dim/bc_rnn.json
+
+#  task: transport
+#    dataset type: ph/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/ph/20_percent/image/bc_rnn.json
+
+#  task: transport
+#    dataset type: ph/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/ph/50_percent/low_dim/bc_rnn.json
+
+#  task: transport
+#    dataset type: ph/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/ph/50_percent/image/bc_rnn.json
+
+#  task: transport
+#    dataset type: mh/20_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/mh/20_percent/low_dim/bc_rnn.json
+
+#  task: transport
+#    dataset type: mh/20_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/mh/20_percent/image/bc_rnn.json
+
+#  task: transport
+#    dataset type: mh/50_percent
+#      hdf5 type: low_dim
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/mh/50_percent/low_dim/bc_rnn.json
+
+#  task: transport
+#    dataset type: mh/50_percent
+#      hdf5 type: image
+python /home/zhangel9/.local/lib/python3.10/site-packages/robomimic-0.3.0-py3.10.egg/robomimic/scripts/train.py --config /home/zhangel9/Working/robomimic/robomimic/exps/templates/dataset_size/transport/mh/50_percent/image/bc_rnn.json
+
